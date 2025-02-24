b4223e7d3b78140275015b0cdc4869bd3ec71c1d
Sebastián Martínez <sebmartinezse@unal.edu.co>
2025-02-23 23:00:14 -0500

    Nodo doble
    Cambiando Nodo por Nodo doble con ciertas modificaciones.

--- Nodo.py
+++ Nodo.py
@@ -1,22 +1,29 @@
-class Nodo():
-    def __init__(self, data, next):
+class nodoDoble:
+    def __init__(self, data, left, right):
         self.__data = data
-        self.__next = next
+        self.__left = left
+        self.__right = right
+        
+    def nodoDoble(self):
+        self.__init__(None, None, None)
     
-    def Nodo(self):
-        self.__init__(None, None)
+    def nodoDoble(self, d):
+        self.__init__(d, None, None)
+        
+    def setData(self, d):
+        self.__data = d
     
-    def Nodo(self, e):
-        self.__init__(e, None)
+    def setLeft(self, n):
+        self.__left = n 
     
-    def setData(self, e):
-        self.__data = e
-    
-    def setNext(self, n):
-        self.__next = n
-    
+    def setRight(self, p):
+        self.__right = p
+        
     def getData(self):
         return self.__data
     
     def getNext(self):
-        return self.__next
+        return self.__left
+    
+    def getPrev(self):
+        return self.__right
